961aa0b90dbf39ec89a8383fd14257b72ea6a91c
SydFloyd <kyleaddison98@gmail.com>
2024-06-23 15:15:56 -0400

    remove usage of key.txt

--- src/openai_init.py
+++ src/openai_init.py
@@ -2,13 +2,10 @@ from openai import OpenAI
 import os
 
 try:
-  openai_key = open("key.txt", 'r').read()
+  openai_key = os.environ.get("OPENAI_API_KEY")
+  assert openai_key != None
 except:
-  try:
-    openai_key = os.environ.get("OPENAI_API_KEY")
-    assert openai_key != None
-  except:
-    raise ValueError("Couldn't resolve openai api key")
+  raise ValueError("Couldn't resolve openai api key")
   
 client = OpenAI(
     api_key = openai_key
